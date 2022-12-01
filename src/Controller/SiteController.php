b28607abef9b83317f9494593d077525907372d1
Nuno Facha <nunofacha@gmail.com>
2022-12-01 17:36:03 -0100

    Add controller and view

---
+++ src/Controller/SiteController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class SiteController extends AbstractController
+{
+    #[Route('/', name: 'index')]
+    public function index(): Response
+    {
+        return $this->render('site/index.html.twig', [
+            'controller_name' => 'SiteController',
+        ]);
+    }
+}
