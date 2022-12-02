1fab2824b1283514200c158366566f2b1d4b98e2
Nuno Facha <nunofacha@gmail.com>
2022-12-02 16:12:04 -0100

    Register UI

--- src/Form/RegistrationFormType.php
+++ src/Form/RegistrationFormType.php
@@ -17,8 +17,18 @@ class RegistrationFormType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('username')
-            ->add('email')
+	        ->add( 'username', null, [
+		        'label' => false,
+		        'attr'  => [
+			        'placeholder' => 'Username',
+		        ],
+	        ] )
+	        ->add( 'email', null, [
+		        'label' => false,
+		        'attr'  => [
+			        'placeholder' => 'Email',
+		        ],
+	        ] )
             ->add('agreeTerms', CheckboxType::class, [
                 'mapped' => false,
                 'constraints' => [
@@ -28,20 +38,21 @@ class RegistrationFormType extends AbstractType
                 ],
             ])
             ->add('plainPassword', PasswordType::class, [
-                // instead of being set onto the object directly,
-                // this is read and encoded in the controller
-                'mapped' => false,
-                'attr' => ['autocomplete' => 'new-password'],
-                'constraints' => [
-                    new NotBlank([
-                        'message' => 'Please enter a password',
-                    ]),
-                    new Length([
-                        'min' => 6,
-                        'minMessage' => 'Your password should be at least {{ limit }} characters',
-                        // max length allowed by Symfony for security reasons
-                        'max' => 4096,
-                    ]),
+	            // instead of being set onto the object directly,
+	            // this is read and encoded in the controller
+	            'mapped'      => false,
+	            'label'       => false,
+	            'attr'        => [ 'autocomplete' => 'new-password', 'placeholder' => 'Password' ],
+	            'constraints' => [
+		            new NotBlank( [
+			            'message' => 'Please enter a password',
+		            ] ),
+		            new Length( [
+			            'min'        => 6,
+			            'minMessage' => 'Your password should be at least {{ limit }} characters',
+			            // max length allowed by Symfony for security reasons
+			            'max'        => 4096,
+		            ] ),
                 ],
             ])
         ;
