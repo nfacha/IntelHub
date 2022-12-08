f1c7dc0e09f721010a846f498bb496a1132c41fd
Nuno Facha <nunofacha@gmail.com>
2022-12-08 15:54:47 -0100

    Add worker

---
+++ migrations/Version20221205150937.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20221205150937 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE ingestor ADD processed_messages INT NOT NULL, ADD total_processed_messages INT NOT NULL, ADD last_message_at DATETIME DEFAULT NULL COMMENT \'(DC2Type:datetime_immutable)\'');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE ingestor DROP processed_messages, DROP total_processed_messages, DROP last_message_at');
+    }
+}
--- src/Entity/Ingestor.php
+++ src/Entity/Ingestor.php
@@ -5,22 +5,21 @@ namespace App\Entity;
 use App\Repository\IngestorRepository;
 use Doctrine\ORM\Mapping as ORM;
 
-#[ORM\Entity(repositoryClass: IngestorRepository::class)]
-class Ingestor
-{
-    #[ORM\Id]
-    #[ORM\GeneratedValue]
-    #[ORM\Column]
-    private ?int $id = null;
+#[ORM\Entity( repositoryClass: IngestorRepository::class )]
+class Ingestor {
+	#[ORM\Id]
+	#[ORM\GeneratedValue]
+	#[ORM\Column]
+	private ?int $id = null;
 
-    #[ORM\Column(length: 255)]
-    private ?string $name = null;
+	#[ORM\Column( length: 255 )]
+	private ?string $name = null;
 
-    #[ORM\Column]
-    private ?int $type = null;
+	#[ORM\Column]
+	private ?int $type = null;
 
-    #[ORM\Column(length: 255, nullable: true)]
-    private ?string $pull_ip = null;
+	#[ORM\Column( length: 255, nullable: true )]
+	private ?string $pull_ip = null;
 
 	#[ORM\Column( length: 255, nullable: true )]
 	private ?string $pull_port = null;
@@ -37,6 +36,15 @@ class Ingestor
 	#[ORM\Column( length: 255, nullable: true )]
 	private ?string $pending_command = null;
 
+	#[ORM\Column]
+	private ?int $processed_messages = null;
+
+	#[ORM\Column]
+	private ?int $total_processed_messages = null;
+
+	#[ORM\Column( nullable: true )]
+	private ?\DateTimeImmutable $last_message_at = null;
+
 	public function getId(): ?int {
 		return $this->id;
 	}
@@ -45,60 +53,51 @@ class Ingestor
 		return $this->name;
 	}
 
-    public function setName(string $name): self
-    {
-        $this->name = $name;
+	public function setName( string $name ): self {
+		$this->name = $name;
 
-        return $this;
-    }
+		return $this;
+	}
 
-    public function getType(): ?int
-    {
-        return $this->type;
-    }
+	public function getType(): ?int {
+		return $this->type;
+	}
 
-    public function setType(int $type): self
-    {
-        $this->type = $type;
+	public function setType( int $type ): self {
+		$this->type = $type;
 
-        return $this;
-    }
+		return $this;
+	}
 
-    public function getPullIp(): ?string
-    {
-        return $this->pull_ip;
-    }
+	public function getPullIp(): ?string {
+		return $this->pull_ip;
+	}
 
-    public function setPullIp(?string $pull_ip): self
-    {
-        $this->pull_ip = $pull_ip;
+	public function setPullIp( ?string $pull_ip ): self {
+		$this->pull_ip = $pull_ip;
 
-        return $this;
-    }
+		return $this;
+	}
 
-    public function getPullPort(): ?string
-    {
-        return $this->pull_port;
-    }
+	public function getPullPort(): ?string {
+		return $this->pull_port;
+	}
 
-    public function setPullPort(?string $pull_port): self
-    {
-        $this->pull_port = $pull_port;
+	public function setPullPort( ?string $pull_port ): self {
+		$this->pull_port = $pull_port;
 
-        return $this;
-    }
+		return $this;
+	}
 
-    public function getPushPort(): ?string
-    {
-        return $this->push_port;
-    }
+	public function getPushPort(): ?string {
+		return $this->push_port;
+	}
 
-    public function setPushPort(?string $push_port): self
-    {
-        $this->push_port = $push_port;
+	public function setPushPort( ?string $push_port ): self {
+		$this->push_port = $push_port;
 
-        return $this;
-    }
+		return $this;
+	}
 
 	public function getSourceType(): ?int {
 		return $this->source_type;
@@ -126,6 +125,36 @@ class Ingestor
 
 	public function setPendingCommand( ?string $pending_command ): self {
 		$this->pending_command = $pending_command;
+
+		return $this;
+	}
+
+	public function getProcessedMessages(): ?int {
+		return $this->processed_messages;
+	}
+
+	public function setProcessedMessages( int $processed_messages ): self {
+		$this->processed_messages = $processed_messages;
+
+		return $this;
+	}
+
+	public function getTotalProcessedMessages(): ?int {
+		return $this->total_processed_messages;
+	}
+
+	public function setTotalProcessedMessages( int $total_processed_messages ): self {
+		$this->total_processed_messages = $total_processed_messages;
+
+		return $this;
+	}
+
+	public function getLastMessageAt(): ?\DateTimeImmutable {
+		return $this->last_message_at;
+	}
+
+	public function setLastMessageAt( ?\DateTimeImmutable $last_message_at ): self {
+		$this->last_message_at = $last_message_at;
 
 		return $this;
 	}
