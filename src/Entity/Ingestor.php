d5821ddb853f34b8afdade744439fe1e2aff0120
Nuno Facha <nunofacha@gmail.com>
2022-12-09 14:21:34 -0100

    Rename field

--- src/Entity/Ingestor.php
+++ src/Entity/Ingestor.php
@@ -8,165 +8,191 @@ use App\Repository\IngestorRepository;
 use DateTimeImmutable;
 use Doctrine\ORM\Mapping as ORM;
 
-#[ORM\Entity( repositoryClass: IngestorRepository::class )]
-class Ingestor {
-	#[ORM\Id]
-	#[ORM\GeneratedValue]
-	#[ORM\Column]
-	private ?int $id = null;
+#[ORM\Entity(repositoryClass: IngestorRepository::class)]
+class Ingestor
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
 
-	#[ORM\Column( length: 255 )]
-	private ?string $name = null;
+    #[ORM\Column(length: 255)]
+    private ?string $name = null;
 
-	#[ORM\Column]
-	private ?int $type = null;
+    #[ORM\Column]
+    private ?int $protocol = null;
 
-	#[ORM\Column( length: 255, nullable: true )]
-	private ?string $pull_ip = null;
+    #[ORM\Column(length: 255, nullable: true)]
+    private ?string $pull_ip = null;
 
-	#[ORM\Column( length: 255, nullable: true )]
-	private ?string $pull_port = null;
+    #[ORM\Column(length: 255, nullable: true)]
+    private ?string $pull_port = null;
 
-	#[ORM\Column( length: 255, nullable: true )]
-	private ?string $push_port = null;
+    #[ORM\Column(length: 255, nullable: true)]
+    private ?string $push_port = null;
 
-	#[ORM\Column]
-	private ?int $source_type = null;
+    #[ORM\Column]
+    private ?int $source_type = null;
 
-	#[ORM\Column]
-	private ?int $active = null;
+    #[ORM\Column]
+    private ?int $active = null;
 
-	#[ORM\Column( length: 255, nullable: true )]
-	private ?string $pending_command = null;
+    #[ORM\Column(length: 255, nullable: true)]
+    private ?string $pending_command = null;
 
-	#[ORM\Column]
-	private ?int $processed_messages = null;
+    #[ORM\Column]
+    private ?int $processed_messages = null;
 
-	#[ORM\Column]
-	private ?int $total_processed_messages = null;
+    #[ORM\Column]
+    private ?int $total_processed_messages = null;
 
-	#[ORM\Column( nullable: true )]
-	private ?DateTimeImmutable $last_message_at = null;
+    #[ORM\Column(nullable: true)]
+    private ?DateTimeImmutable $last_message_at = null;
 
-	public function getId(): ?int {
-		return $this->id;
-	}
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
 
-	public function getName(): ?string {
-		return $this->name;
-	}
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
 
-	public function setName( string $name ): self {
-		$this->name = $name;
+    public function setName(string $name): self
+    {
+        $this->name = $name;
 
-		return $this;
-	}
+        return $this;
+    }
 
-	public function getType(): ?int {
-		return $this->type;
-	}
+    public function getProtocol(): ?int
+    {
+        return $this->protocol;
+    }
 
-	public function setType( int $type ): self {
-		$this->type = $type;
+    public function setProtocol(int $protocol): self
+    {
+        $this->protocol = $protocol;
 
-		return $this;
-	}
+        return $this;
+    }
 
-	public function getPullIp(): ?string {
-		return $this->pull_ip;
-	}
+    public function getPullIp(): ?string
+    {
+        return $this->pull_ip;
+    }
 
-	public function setPullIp( ?string $pull_ip ): self {
-		$this->pull_ip = $pull_ip;
+    public function setPullIp(?string $pull_ip): self
+    {
+        $this->pull_ip = $pull_ip;
 
-		return $this;
-	}
+        return $this;
+    }
 
-	public function getPullPort(): ?string {
-		return $this->pull_port;
-	}
+    public function getPullPort(): ?string
+    {
+        return $this->pull_port;
+    }
 
-	public function setPullPort( ?string $pull_port ): self {
-		$this->pull_port = $pull_port;
+    public function setPullPort(?string $pull_port): self
+    {
+        $this->pull_port = $pull_port;
 
-		return $this;
-	}
+        return $this;
+    }
 
-	public function getPushPort(): ?string {
-		return $this->push_port;
-	}
+    public function getPushPort(): ?string
+    {
+        return $this->push_port;
+    }
 
-	public function setPushPort( ?string $push_port ): self {
-		$this->push_port = $push_port;
+    public function setPushPort(?string $push_port): self
+    {
+        $this->push_port = $push_port;
 
-		return $this;
-	}
+        return $this;
+    }
 
-	public function getSourceType(): ?int {
-		return $this->source_type;
-	}
+    public function getSourceType(): ?int
+    {
+        return $this->source_type;
+    }
 
-	public function setSourceType( int $source_type ): self {
-		$this->source_type = $source_type;
+    public function setSourceType(int $source_type): self
+    {
+        $this->source_type = $source_type;
 
-		return $this;
-	}
+        return $this;
+    }
 
-	public function getActive(): ?int {
-		return $this->active;
-	}
+    public function getActive(): ?int
+    {
+        return $this->active;
+    }
 
-	public function setActive( int $active ): self {
-		$this->active = $active;
+    public function setActive(int $active): self
+    {
+        $this->active = $active;
 
-		return $this;
-	}
+        return $this;
+    }
 
-	public function getPendingCommand(): ?string {
-		return $this->pending_command;
-	}
+    public function getPendingCommand(): ?string
+    {
+        return $this->pending_command;
+    }
 
-	public function setPendingCommand( ?string $pending_command ): self {
-		$this->pending_command = $pending_command;
+    public function setPendingCommand(?string $pending_command): self
+    {
+        $this->pending_command = $pending_command;
 
-		return $this;
-	}
+        return $this;
+    }
 
-	public function getProcessedMessages(): ?int {
-		return $this->processed_messages;
-	}
+    public function getProcessedMessages(): ?int
+    {
+        return $this->processed_messages;
+    }
 
-	public function setProcessedMessages( int $processed_messages ): self {
-		$this->processed_messages = $processed_messages;
+    public function setProcessedMessages(int $processed_messages): self
+    {
+        $this->processed_messages = $processed_messages;
 
-		return $this;
-	}
+        return $this;
+    }
 
-	public function getTotalProcessedMessages(): ?int {
-		return $this->total_processed_messages;
-	}
+    public function getTotalProcessedMessages(): ?int
+    {
+        return $this->total_processed_messages;
+    }
 
-	public function setTotalProcessedMessages( int $total_processed_messages ): self {
-		$this->total_processed_messages = $total_processed_messages;
+    public function setTotalProcessedMessages(int $total_processed_messages): self
+    {
+        $this->total_processed_messages = $total_processed_messages;
 
-		return $this;
-	}
+        return $this;
+    }
 
-	public function getLastMessageAt(): ?DateTimeImmutable {
-		return $this->last_message_at;
-	}
+    public function getLastMessageAt(): ?DateTimeImmutable
+    {
+        return $this->last_message_at;
+    }
 
-	public function setLastMessageAt( ?DateTimeImmutable $last_message_at ): self {
-		$this->last_message_at = $last_message_at;
+    public function setLastMessageAt(?DateTimeImmutable $last_message_at): self
+    {
+        $this->last_message_at = $last_message_at;
 
-		return $this;
-	}
+        return $this;
+    }
 
-	public function getTypeLabel() {
-		return MessageProtocol::getLabel()[ $this->type ];
-	}
+    public function getTypeLabel()
+    {
+        return MessageProtocol::getLabel()[$this->protocol];
+    }
 
-	public function getSourceTypeLabel() {
-		return SourceType::getLabel()[ $this->source_type ];
-	}
+    public function getSourceTypeLabel()
+    {
+        return SourceType::getLabel()[$this->source_type];
+    }
 }
